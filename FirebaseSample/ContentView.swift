f2ebc492282874ea032040ed76f6a40d3a708b80
中岡黎 <reideeplearning@gmail.com>
2019-07-11 14:14:29 +0900

    Initial Commit

---
+++ FirebaseSample/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  FirebaseSample
+//
+//  Created by 中岡黎 on 2019/07/11.
+//  Copyright © 2019 NakaokaRei. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
