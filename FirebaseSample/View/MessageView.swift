9f9c1bab037701955707b1bd153a270656e10a9f
中岡黎 <reideeplearning@gmail.com>
2019-07-14 22:19:57 +0900

    messageviewの追加

---
+++ FirebaseSample/View/MessageView.swift
@@ -0,0 +1,45 @@
+//
+//  MessageView.swift
+//  FirebaseSample
+//
+//  Created by 中岡黎 on 2019/07/14.
+//  Copyright © 2019 NakaokaRei. All rights reserved.
+//
+
+import SwiftUI
+
+struct MessageView : View {
+    @EnvironmentObject private var firebaseModel: FirebaseViewModel
+    @State private var message: String = ""
+    var body: some View {
+        HStack {
+            TextField($message, placeholder: Text("message")){
+                UIApplication.shared.keyWindow?.endEditing(true)
+            }
+            .frame(height: 40)
+            .padding(.horizontal, 8)
+            .border(Color.gray, cornerRadius: 8)
+            
+            Image(systemName: "plus.circle").imageScale(.large)
+                .tapAction {
+                    guard !self.message.isEmpty else {
+                        return
+                    }
+                    self.firebaseModel.add(name: self.message)
+                    self.clear()
+            }
+        }
+    }
+    
+    private func clear() {
+        message = ""
+    }
+}
+
+#if DEBUG
+struct MessageView_Previews : PreviewProvider {
+    static var previews: some View {
+        MessageView().environmentObject(FirebaseViewModel())
+    }
+}
+#endif
